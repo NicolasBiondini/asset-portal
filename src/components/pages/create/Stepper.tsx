a73cb7619963add4d535d934152c72470982a9ce
NicolasBiondini <nicolasbiondini29@gmail.com>
2024-06-17 18:39:45 -0300

    feature: adding stepper functionality

--- src/components/pages/create/Stepper.tsx
+++ src/components/pages/create/Stepper.tsx
@@ -5,33 +5,61 @@ import {
   useStepper,
 } from "@/components/ui/stepper";
 import { Button } from "@/components/ui/button";
+import {
+  HandCoins,
+  HomeIcon,
+  Landmark,
+  PackagePlus,
+  Wallet,
+} from "lucide-react";
+import { useEffect } from "react";
+import { useWalletState } from "@/data/wallet/storage";
+import AddAddressModal from "@/components/modals/AddAddressModal";
 
 const steps = [
-  { label: "Step 1" },
-  { label: "Step 2" },
-  { label: "Step 3" },
+  { label: "Info", icon: PackagePlus },
+  { label: "Config", icon: Landmark },
+  { label: "Mint", icon: HandCoins },
 ] satisfies StepItem[];
 
 export default function StepperCreateAsset() {
+  const { walletList, address } = useWalletState();
+
   return (
     <div className="flex w-full flex-col gap-4">
       <Stepper initialStep={0} steps={steps}>
         {steps.map((stepProps, index) => {
           return (
             <Step key={stepProps.label} {...stepProps}>
-              <div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
+              <div className="h-full flex items-center justify-start p-6 border bg-secondary text-primary rounded-md">
                 <h1 className="text-xl">Step {index + 1}</h1>
               </div>
             </Step>
           );
         })}
-        <Footer />
+        {walletList.length === 0 ||
+        walletList.filter((wallet) => wallet.address !== address).length !==
+          0 ? (
+          <div className="flex w-full justify-end items-end">
+            <AddAddressModal>
+              <Button
+                size={"sm"}
+                className="font-unbounded font-bold text-xs hover:scale-[102%] flex gap-2 transition-all"
+              >
+                <Wallet className="w-3 h-3 " />
+                <p>Connect</p>
+              </Button>
+            </AddAddressModal>
+          </div>
+        ) : (
+          <Footer isDisabled={false} />
+        )}
       </Stepper>
     </div>
   );
 }
 
-const Footer = () => {
+const Footer = ({ isDisabled }: { isDisabled?: boolean }) => {
   const {
     nextStep,
     prevStep,
@@ -40,7 +68,9 @@ const Footer = () => {
     isLastStep,
     isOptionalStep,
     isDisabledStep,
+    currentStep,
   } = useStepper();
+
   return (
     <>
       {hasCompletedAllSteps && (
@@ -60,11 +90,21 @@ const Footer = () => {
               onClick={prevStep}
               size="sm"
               variant="secondary"
+              className="font-unbounded font-bold text-xs hover:scale-[102%] transition-all"
             >
               Prev
             </Button>
-            <Button size="sm" onClick={nextStep}>
-              {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+            <Button
+              size="sm"
+              className="font-unbounded font-bold text-xs hover:scale-[102%] transition-all"
+              onClick={nextStep}
+              disabled={isDisabled}
+            >
+              {isLastStep
+                ? "Create and Mint 🎉"
+                : isOptionalStep
+                ? "Skip"
+                : "Next"}
             </Button>
           </>
         )}
