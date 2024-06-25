22c16dc6b1ee0d17ff5425f4f8320f6a763378e1
NicolasBiondini <nicolasbiondini29@gmail.com>
2024-06-24 23:00:00 -0300

    feature: about us + background color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="overflow-y-auto">
+      <body className="overflow-y-auto bg-colors-bg-main">
         <Main />
         <NextScript />
       </body>
