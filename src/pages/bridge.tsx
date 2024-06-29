9909a2d15f5c365308c8d823f1bf6247cb1a3b47
NicolasBiondini <nicolasbiondini29@gmail.com>
2024-06-29 05:11:29 -0300

    feature: text bridge

--- src/pages/bridge.tsx
+++ src/pages/bridge.tsx
@@ -11,8 +11,15 @@ function bridge({}: Props) {
           <h1 className="text-colors-pink-dot text-xl font-bold font-unbounded">
             Bridge
           </h1>
-          <p className="text-xs">Polkadot AssetHub bridge to Kusama AssetHub</p>
-          <p className="text-xs">{"Discover "}</p>
+          <p className="text-xs">
+            The Polkadot-Kusama bridge enables seamless asset transfers between
+            interconnected parachains within the Polkadot and Kusama ecosystems.{" "}
+          </p>
+          <p className="text-xs">
+            {
+              "Explore effortless asset movement across these networks, leveraging the bridge infrastructure."
+            }
+          </p>
         </div>
         <div className="flex flex-col w-full h-[80%] items-center justify-start mt-auto  ">
           <WIP number={3} />
